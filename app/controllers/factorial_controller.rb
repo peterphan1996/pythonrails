850542e8cb2b4e09c2de4036e02d91f2429443bd
peterphan1996 <peterphan_1996@live.com>
2018-03-28 15:23:16 +0700

    Testing threading

--- app/controllers/factorial_controller.rb
+++ app/controllers/factorial_controller.rb
@@ -4,7 +4,16 @@ class FactorialController < ApplicationController
     end
 
     def calculate
-        @u = `python3.6 /home/ubuntu/pythonrails/app/controllers/factorial.py #{params[:input][:number]}`
+        threads = []
+
+        500.times do |i|
+            threads << Thread.new do     
+            @u = `python3.6 /home/peter/Documents/PPL/Lab3/test/abc/app/controllers/factorial.py #{params[:input][:number]}`
+            puts i
+            sleep 2.minutes
+            end
+        end
+        threads.map(&:join)
         render 'hello'
     end
 
